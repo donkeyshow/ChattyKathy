d736194a0b3a98cca5dab3e520760fa29026f197
donkeyshow <pitodeburro@gmail.com>
2014-03-06 21:02:30 -0800

    Update Message.cpp

--- Message.cpp
+++ Message.cpp
@@ -1,11 +1,15 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS // this disables secure warning errors.  it was easier than finding another way to write the time code
+#endif
+
 #include "Message.h"
 
-string Message::_logFilePath = string();
+string Message::_logFilePath = string(); // initialize static members
 int Message::_consoleBuffer = 0;
 
 Message::Message()
 {
-
+	// default constructor does nothing
 }
 
 const string Message::GetChatMessage()
@@ -20,11 +24,25 @@ void Message::SetMessage( string message )
 
 void Message::AppendToLog()
 {
+	std::stringstream stringBuffer; // create a stringstream object to insert time
 	std::fstream fs; // create a file stream object
+	string logString, tempString; // create a string object to get stringstream data
 
-	fs.open(Message::_logFilePath.c_str(), std::fstream::app); // open the shell file for read write and append
+	using std::chrono::system_clock; // make system_clock available for use
+	std::time_t tt = system_clock::to_time_t(system_clock::now()); // get now and put it in a time_t object
+	struct tm* newtime = std::localtime(&tt); // create a tm object and store the localtime tt in it
 
-	fs << this->_message << endl; // output song path to file
+	stringBuffer << std::put_time(newtime, "%H:%M:%S") << " " << std::put_time(newtime, "%x"); // put the local time in the string stream
+	stringBuffer >> std::noskipws; // this doesn't fucking work
+	stringBuffer >> logString >> std::ws >> tempString; // insert the stringstream into logString
+	logString.append(" "); // add a space
+	logString.append(tempString);
+	logString.append(" ");	
+	logString.append(this->_message); // add the message
+	
+	fs.open(Message::_logFilePath.c_str(), std::fstream::app); // open the shell file for append
+
+	fs << logString.c_str() << endl; // output song path to file
 
 	fs.close();
 	fs.clear(std::ios_base::goodbit);
@@ -32,10 +50,10 @@ void Message::AppendToLog()
 
 void Message::PrintToConsole()
 {
-	cout << this->_message << endl;
+	cout << this->_message << endl; // output current message
 }
 
 Message::~Message()
 {
-
+	// default destructor does nothing
 }
